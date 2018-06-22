5cb5aa8afad0ea241d11826239a289551ba7425c
bergpb <lindembergdepaulo@gmail.com>
2018-06-22 14:39:00 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-navigation-drawer :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app>
+      <v-list>
+        <v-list-tile>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="red accent-4" app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <!--<v-footer :fixed="fixed" app>
+      <span> WorldCup Results - Made with love by bergpb</span>
+    </v-footer>-->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: false,
+      rightDrawer: false,
+      title: 'WorldCup Results 2018'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-container fluid grid-list-lg grid-list-sm>
+      <v-slide-y-transition mode="out-in">
+        <v-layout row wrap>
+          <v-flex xs12 v-for="worldcup in worldcups" :key="worldcup.id">
+            <v-card class="default" color="blue-grey lighten-4">
+              <v-card-title primary-title>
+                <div class="headline">{{worldcup.countries}}</div>
+                <v-card-text>
+                   <v-icon>poll</v-icon> {{worldcup.result}}<br>
+                   <v-icon>timer</v-icon> {{worldcup.status}}<br>
+                   <v-icon>stars</v-icon> {{worldcup.winner}}<br>
+                   <v-icon>calendar_today</v-icon> {{worldcup.date}}<br>
+                   <v-icon>location_on</v-icon> {{worldcup.localization}}<br>
+                </v-card-text>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-slide-y-transition>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      worldcups: {}
+    }
+  },
+  mounted () {
+    fetch('https://worldcupresults.herokuapp.com/api/worldcup')
+      .then(response => response.json())
+      .then((res) => {
+        this.worldcups = res.worldcup.reverse()
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
