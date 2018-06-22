90170ffbd16fb5134592b4bea6fcf1d9800abe39
bergpb <lindembergdepaulo@gmail.com>
2018-06-22 16:36:45 -0300

    loading implementado

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,37 @@
 <template>
-  <v-card>
-    <v-container fluid grid-list-lg grid-list-sm>
+  <div v-if="loading">
+    <v-container fill-height>
+      <v-layout row wrap>
+        <v-flex class="text-xs-center">
+          <v-progress-circular :size="70" :width="7" indeterminate color="red"></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+  <div v-else-if="errored">
+    <v-container fill-height>
+      <v-layout row wrap>
+        <v-flex class="text-xs-center">
+          <h2>Ops encontramos um erro!</h2>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+  <v-card v-else>
+    <v-container fluid grid-list-lg>
       <v-slide-y-transition mode="out-in">
         <v-layout row wrap>
-          <v-flex v-for="worldcup in worldcups" :key="worldcup.id" xs12>
-            <v-card class="default" color="blue-grey lighten-4">
+          <v-flex v-for="worldcup in worldcups" :key="worldcup.id" xs6>
+            <v-card class="default" color="blue-grey lighten-4" height="100%">
               <v-card-title primary-title>
-                <div v-if="errored">
-                  <p>We're sorry, we're not able to retrieve this information at the moment, please try back later.</p>
-                </div>
-                <div v-else>
-                  <div v-if="loading">
-                    <v-progress-circular :size="70" :width="7" indeterminate color="red"></v-progress-circular>
-                  </div>
-                  <div v-else>
-                    <div class="headline">{{worldcup.countries}}</div>
-                    <v-card-text>
-                       <v-icon>poll</v-icon> {{worldcup.result}}<br>
-                       <v-icon>timer</v-icon> {{worldcup.status}}<br>
-                       <v-icon>stars</v-icon> {{worldcup.winner}}<br>
-                       <v-icon>calendar_today</v-icon> {{worldcup.date}}<br>
-                       <v-icon>location_on</v-icon> {{worldcup.localization}}<br>
-                    </v-card-text>
-                  </div>
-                </div>
+                <div class="headline">{{worldcup.countries}}</div>
+                <v-card-text>
+                   <v-icon>poll</v-icon> {{worldcup.result}}<br>
+                   <v-icon>timer</v-icon> {{worldcup.status}}<br>
+                   <v-icon>stars</v-icon> {{worldcup.winner}}<br>
+                   <v-icon>calendar_today</v-icon> {{worldcup.date}}<br>
+                   <v-icon>location_on</v-icon> {{worldcup.localization}}<br>
+                </v-card-text>
               </v-card-title>
             </v-card>
           </v-flex>
@@ -39,7 +47,8 @@ export default {
     return {
       worldcups: null,
       loading: true,
-      errored: false
+      errored: false,
+      error: null
     }
   },
   mounted () {
@@ -48,8 +57,9 @@ export default {
       .then((res) => {
         this.worldcups = res.worldcup.reverse()
       })
-      .catch(error => {
-        console.log(error)
+      .catch(err => {
+        this.error = err
+        console.log('Erro: ' + err)
         this.errored = true
       })
       .finally(() => this.loading = false)
