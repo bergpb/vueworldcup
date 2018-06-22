96e1440e826cb29a353f05c286381b77159ce94f
bergpb <lindembergdepaulo@gmail.com>
2018-06-22 16:00:12 -0300

    tentativa de adicionar o loading

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,27 @@
     <v-container fluid grid-list-lg grid-list-sm>
       <v-slide-y-transition mode="out-in">
         <v-layout row wrap>
-          <v-flex xs12 v-for="worldcup in worldcups" :key="worldcup.id">
+          <v-flex v-for="worldcup in worldcups" :key="worldcup.id" xs12>
             <v-card class="default" color="blue-grey lighten-4">
               <v-card-title primary-title>
-                <div class="headline">{{worldcup.countries}}</div>
-                <v-card-text>
-                   <v-icon>poll</v-icon> {{worldcup.result}}<br>
-                   <v-icon>timer</v-icon> {{worldcup.status}}<br>
-                   <v-icon>stars</v-icon> {{worldcup.winner}}<br>
-                   <v-icon>calendar_today</v-icon> {{worldcup.date}}<br>
-                   <v-icon>location_on</v-icon> {{worldcup.localization}}<br>
-                </v-card-text>
+                <div v-if="errored">
+                  <p>We're sorry, we're not able to retrieve this information at the moment, please try back later.</p>
+                </div>
+                <div v-else>
+                  <div v-if="loading">
+                    <v-progress-circular :size="70" :width="7" indeterminate color="red"></v-progress-circular>
+                  </div>
+                  <div v-else>
+                    <div class="headline">{{worldcup.countries}}</div>
+                    <v-card-text>
+                       <v-icon>poll</v-icon> {{worldcup.result}}<br>
+                       <v-icon>timer</v-icon> {{worldcup.status}}<br>
+                       <v-icon>stars</v-icon> {{worldcup.winner}}<br>
+                       <v-icon>calendar_today</v-icon> {{worldcup.date}}<br>
+                       <v-icon>location_on</v-icon> {{worldcup.localization}}<br>
+                    </v-card-text>
+                  </div>
+                </div>
               </v-card-title>
             </v-card>
           </v-flex>
@@ -27,7 +37,9 @@
 export default {
   data () {
     return {
-      worldcups: {}
+      worldcups: null,
+      loading: true,
+      errored: false
     }
   },
   mounted () {
@@ -36,11 +48,15 @@ export default {
       .then((res) => {
         this.worldcups = res.worldcup.reverse()
       })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
