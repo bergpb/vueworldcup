5f84a6eb8b1af8d1679724dfcaf868ea573cb4b1
bergpb <lindembergdepaulo@gmail.com>
2018-07-01 08:05:40 -0300

    mudanca no menu

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <v-app>
+    <v-layout wrap>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="@/assets/worldcup_logo.png" >
+            </v-list-tile-avatar>
+  
+            <v-list-tile-content>
+              <v-list-tile-title>Copa do Mundo 2018</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+  
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+  
+          <v-list-tile v-for="item in items" :key="item.title" :to="item.path">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+  
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
     <v-toolbar color="indigo darken-2" app>
-      <v-avatar>
-        <img src="@/assets/worldcup_logo.png">
-      </v-avatar>
+      <v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Resultados da Copa</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn to="/current" flat>
-          Em Andamento
-        </v-btn>
-        <v-btn to="/last" flat>
-          Últimas
-        </v-btn>
-        <v-btn to="/future" flat>
-          Futuras
-        </v-btn>
-      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -36,7 +53,14 @@ export default {
   data () {
     return {
       title: 'WorldCup 2018',
-      fixed: true
+      fixed: true,
+      drawer: null,
+        items: [
+          { title: 'Início', icon: 'home', path: '/' },
+          { title: 'Em Andamento', icon: 'timer', path: '/current' },
+          { title: 'Últimos Jogos', icon: 'keyboard_arrow_left', path: '/last' },
+          { title: 'Jogos Futuros', icon: 'keyboard_arrow_right', path: '/future' },
+        ]
     }
   },
   name: 'App'
